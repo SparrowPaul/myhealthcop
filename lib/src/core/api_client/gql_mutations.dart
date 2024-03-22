251d4fa37290348f149388cdfe64cf1f89c9d118
Paul Agbadi <paul.agbadi@wayamoney.com>
2024-03-23 02:05:43 +0800

    add gpl mutations

---
+++ lib/src/core/api_client/gql_mutations.dart
@@ -0,0 +1,66 @@
+String loginMutation(String email, String password, String userType) {
+    return '''
+      mutation Login {
+          login(payload: { email: null, password: null }, userType: null) {
+              responseMessage
+              data {
+                  accessToken
+                  refreshToken
+              }
+              responseStatus {
+                  errorCode
+                  statusCode
+                  isSuccess
+              }
+          }
+      }
+      ''';
+  }
+
+String registrationMutation(String email, String password, String userType) {
+    return '''
+      mutation Register {
+          register(payload: { email: $email, password: $password, userType: $userType }) {
+              data {
+                  id
+                  userType
+                  email
+                  firstName
+                  lastName
+              }
+              responseStatus {
+                  errorCode
+                  statusCode
+                  isSuccess
+              }
+              responseMessage
+          }
+      }
+      ''';
+  }
+
+String forgotPasswordMutation(String email) {
+  return '''
+    mutation ForgotPassword {
+        forgotPassword(email: $email) {
+            responseMessage
+            responseStatus {
+                statusCode
+            }
+        }
+    }
+    ''';
+}
+
+String resetPasswordMutation(String token, String uid, String newPassword) {
+  return '''
+    mutation ResetPassword {
+      resetPassword(payload: { token: $token, newPassword: $newPassword, uid: $uid }) {
+          responseStatus {
+              statusCode
+          }
+          responseMessage
+      }
+    }
+  ''';
+}
