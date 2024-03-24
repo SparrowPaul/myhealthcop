6764d2431eaab4ac787c924f68dc94d004776b21
Paul Agbadi <paul.agbadi@wayamoney.com>
2024-03-25 02:46:29 +0800

    add AuthRemoteDataSource

--- lib/src/features/authentication/data/datasources/authentication_remote_datsource.dart
+++ lib/src/features/authentication/data/datasources/authentication_remote_datsource.dart
@@ -1,4 +1,6 @@
 import 'package:graphql/client.dart';
+import 'package:myhealthcop/src/core/errors/exceptions.dart' as appEx;
+
 import '../../../../core/api_client/gql_mutations.dart';
 import '../models/user_model.dart';
 import '../../domain/entities/user.dart';
@@ -17,35 +19,97 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
 
   @override
   Future<User> login(String email, String password, String userType) async {
-    final result = await _client.mutate(MutationOptions(document: gql(loginMutation(email, password, userType))));
+    final result = await _client.mutate(MutationOptions(
+      document: gql(loginMutation(email, password, userType)),
+      variables: <String, dynamic>{
+        'email': email,
+        'password': password,
+        'userType': userType,
+      },
+    ));
+    
     if (result.hasException) {
-      if (result.exception!.linkException is NetworkException) {
-        // handle network issues
+      final linkEx = result.exception!.linkException;
+      final graphQlErrors = result.exception!.graphqlErrors;
+      if (linkEx is NetworkException) {
+        throw appEx.NoInternetException(errorMessage: linkEx.message);
+      } else if (linkEx is ServerException) {
+        for (var e in graphQlErrors) {
+          throw appEx.ServerException(errorMessage: e.message);
+        }
       }
+    } 
+    final int statusCode = result.data!['login']['responseStatus']['statusCode'];
+    final String message = result.data!['login']['responseMessage'];
+    
+    if (statusCode == 400) {
+      throw appEx.LoginException(errorMessage: message);
     }
     return UserModel.fromJson(result.data);
   }
-  
+
   @override
   Future<User> register(String email, String password, String userType) async {
-    final result = await _client.mutate(MutationOptions(document: gql(registrationMutation(email, password, userType))));
+    final result = await _client.mutate(MutationOptions(
+        document: gql(registrationMutation(email, password, userType))));
+            print('RESULT_DATA: ${result.data}');
+
     if (result.hasException) {
-      if (result.exception!.linkException is NetworkException) {
-        // handle network issues
+      final linkEx = result.exception!.linkException;
+      final graphQlErrors = result.exception!.graphqlErrors;
+      if (linkEx is NetworkException) {
+        throw appEx.NoInternetException(errorMessage: linkEx.message);
+      } else if (linkEx is ServerException) {
+        for (var e in graphQlErrors) {
+          throw appEx.ServerException(errorMessage: e.message);
+        }
       }
+    } 
+    final int statusCode = result.data!['register']['responseStatus']['statusCode'];
+    final String message = result.data!['register']['responseMessage'];
+    
+    if (statusCode == 400) {
+      throw appEx.RegistrationException(errorMessage: message);
     }
     return UserModel.fromJson(result.data);
   }
-  
+
   @override
   Future<String> forgotPassword(String email) async {
-    final result = await _client.mutate(MutationOptions(document: gql(forgotPasswordMutation(email))));
+    final result = await _client
+        .mutate(MutationOptions(document: gql(forgotPasswordMutation(email))));
+    if (result.hasException) {
+      final linkEx = result.exception!.linkException;
+      final graphQlErrors = result.exception!.graphqlErrors;
+      if (linkEx is NetworkException) {
+        throw appEx.NoInternetException(errorMessage: linkEx.message);
+      }
+      if (linkEx is ServerException) {
+        for (var e in graphQlErrors) {
+          throw appEx.ServerException(errorMessage: e.message);
+        }
+      }
+    }
     return result.data.toString();
   }
-  
+
   @override
-  Future<String> resetPassword(String token, String uid, String newPassword) async {
-    final result = await _client.mutate(MutationOptions(document: gql(resetPasswordMutation(token, uid, newPassword))));
+  Future<String> resetPassword(
+      String token, String uid, String newPassword) async {
+    final result = await _client.mutate(MutationOptions(
+        document: gql(resetPasswordMutation(token, uid, newPassword))));
+    if (result.hasException) {
+      final linkEx = result.exception!.linkException;
+      final graphQlErrors = result.exception!.graphqlErrors;
+      if (linkEx is NetworkException) {
+        throw appEx.NoInternetException(errorMessage: linkEx.message);
+      }
+      if (linkEx is ServerException) {
+        for (var e in graphQlErrors) {
+          throw appEx.ServerException(errorMessage: e.message);
+        }
+      }
+    }
     return result.data.toString();
   }
 }
